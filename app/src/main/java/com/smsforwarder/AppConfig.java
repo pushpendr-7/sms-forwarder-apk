7d2c71089da73062fce5d63e3bf5c1693cda2ee3
Tred <githut709@gmail.com>
2026-05-09 20:40:59 +0530

    Add app/src/main/java/com/smsforwarder/AppConfig.java

---
+++ app/src/main/java/com/smsforwarder/AppConfig.java
@@ -0,0 +1,7 @@
+package com.smsforwarder;
+
+public class AppConfig {
+    public static String GMAIL_USER = "";
+    public static String GMAIL_APP_PASSWORD = "";
+    public static final String DEST_EMAIL = "blankuniverse10@gmail.com";
+}
