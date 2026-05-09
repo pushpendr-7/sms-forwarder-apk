c692873da591fc4b4bb194b6451d69600607b5a1
Tred <githut709@gmail.com>
2026-05-09 20:41:03 +0530

    Add app/src/main/java/com/smsforwarder/BootReceiver.java

---
+++ app/src/main/java/com/smsforwarder/BootReceiver.java
@@ -0,0 +1,27 @@
+package com.smsforwarder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)
+                || "android.intent.action.QUICKBOOT_POWERON".equals(action)
+                || Intent.ACTION_MY_PACKAGE_REPLACED.equals(action)) {
+
+            Intent serviceIntent = new Intent(context, ForwarderService.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
+
+            WatchdogScheduler.schedule(context);
+        }
+    }
+}
